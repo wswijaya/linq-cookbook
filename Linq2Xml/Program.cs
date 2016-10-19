680dc96a8cd17e9d70c44db53d2ebdc9cb18ad4e
Wongso Wijaya <wswijaya@hotmail.com>
2016-10-20 01:08:18 +0800

    Added LINQ to XML update, transform , and query

--- Linq2Xml/Program.cs
+++ Linq2Xml/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,15 +29,47 @@ namespace Linq2Xml
             using (XmlReader reader = XmlReader.Create(@"../../data/Course.xml", settings))
             {
                 var course = XElement.Load(reader);
-                var result = (from l in course.Elements("lesson")
-                    where l.Attribute("id").Value.Equals("1")
-                    select l).First().Attributes();
+                //var result = (from l in course.Elements("lesson")
+                //    where l.Attribute("id").Value.Equals("1")
+                //    select l).First().Attributes();
 
-                foreach (var c in result)
+                //foreach (var l in result)
+                //{
+                //    Console.WriteLine(l);
+                //}
+
+                var result =
+                    course.Descendants().Elements("description").Ancestors("lesson").Distinct().Attributes("title");
+                foreach (var l in result)
                 {
-                    Console.WriteLine(c);
+                    Console.WriteLine(l);
                 }
-            } 
+
+                var lesson = course.Elements("lesson").Where(l => l.Attribute("id").Value == "2").First();
+                var newSubLesson = XElement.Parse(@"<sublesson id=""2.6"" title=""A bonus point"" />");
+                lesson.Add(newSubLesson);
+                var newLesson = new XElement("lesson", 
+                    new XAttribute("id", "3"),
+                    new XAttribute("title", "Standard Query Operators Part 1"));
+                lesson.AddAfterSelf(newLesson);
+                Console.WriteLine(course.ToString());
+
+                var course2 = new XElement("course", from l in course.Elements("lesson")
+                                                     select new XElement("lesson", l.Attributes(),
+                                                     from sl in l.Elements("sublesson")
+                                                     select new XElement("sublesson", 
+                                                     from a in sl.Attributes()
+                                                     select new XElement(a.Name, a.Value))));
+                Console.WriteLine(course2);
+
+                var course3 = new XElement("course", from l in course2.Elements("lesson")
+                                                     select new XElement("lesson", l.Attributes(),
+                                                     from sl in l.Elements("sublesson")
+                                                     select new XElement("sublesson", 
+                                                     from a in sl.Descendants()
+                                                     select new XAttribute(a.Name, a.Value))));
+                Console.WriteLine(course3);
+            }
 
             string rawXmlData = System.IO.File.ReadAllText(@"../../data/PurchaseOrder.xml");
             XElement purchaseOrders = XElement.Parse(rawXmlData);
@@ -50,5 +83,11 @@ namespace Linq2Xml
 
             Console.ReadKey();
         }
+    }
+
+    public class LessonNode
+    {
+        public string id { get; set; }
+        public string title { get; set; }
     }
 }
